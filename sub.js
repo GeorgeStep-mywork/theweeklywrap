871279fef7eeb7b2f9c52e3334ebaf273f97033e
GeorgeStep-mywork <71914792+GeorgeStep-mywork@users.noreply.github.com>
2020-12-01 15:10:44 +0530

    Just trying new stuff

--- sub.js
+++ sub.js
@@ -11,3 +11,20 @@ line2.addEventListener('animationend', function(e) {
     card.classList.add('done');
   }, 1000);
 });
+
+function ValidateForm(frm) {
+if (frm.Name.value == "") {alert('Name is required.');frm.Name.focus();return false;}
+if (frm.Organization.value == "") {alert('Organization is required.');frm.Organization.focus();return false;}
+if (frm.FromEmailAddress.value == "") {alert('Email address is required.');frm.FromEmailAddress.focus();return false;}
+if (frm.FromEmailAddress.value.indexOf("@") < 1 || frm.FromEmailAddress.value.indexOf(".") < 1) {alert('Please enter a valid email address.');frm.FromEmailAddress.focus();return false;}
+if (frm.skip_CaptchaCode.value == "") {alert('Enter security code.');frm.skip_CaptchaCode.focus();return false;}
+return true; }
+function ReloadCaptchaImage(captchaImageId) {
+var obj = document.getElementById(captchaImageId);
+var src = '' + obj.src;
+obj.src = '';
+var date = new Date();
+var pos = src.indexOf('&rad=');
+if (pos >= 0) { src = src.substr(0, pos); }
+obj.src = src + '&rad=' + date.getTime();
+return false; }
