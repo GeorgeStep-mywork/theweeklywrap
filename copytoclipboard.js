757b2d346d8c694e6e7431b2e87608926dc0cea0
GeorgeStep-mywork <71914792+GeorgeStep-mywork@users.noreply.github.com>
2021-01-09 14:16:16 +0530

    Made corrections to the copy to clipboard button

--- copytoclipboard.js
+++ copytoclipboard.js
@@ -12,3 +12,18 @@ function copyToClip() {
   /* Alert the copied text */
 //   alert("Copied the text: " + copyText.value);
 }
+
+function myFunction() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
+}
