2852c441090762dd3629bae8ae74f1673a5d4306
GeorgeStep-mywork <71914792+GeorgeStep-mywork@users.noreply.github.com>
2021-03-19 14:06:16 +0530

    Added Post 18 on Gratitude

--- sub.js
+++ sub.js
@@ -28,3 +28,24 @@ var pos = src.indexOf('&rad=');
 if (pos >= 0) { src = src.substr(0, pos); }
 obj.src = src + '&rad=' + date.getTime();
 return false; }
+
+
+var cta = document.querySelector(".cta");
+var check = 0;
+
+cta.addEventListener('click', function(e){
+    var text = e.target.nextElementSibling;
+    var loginText = e.target.parentElement;
+    text.classList.toggle('show-hide');
+    loginText.classList.toggle('expand');
+    if(check == 0)
+    {
+        cta.innerHTML = "<i class=\"fas fa-chevron-up\"></i>";
+        check++;
+    }
+    else
+    {
+        cta.innerHTML = "<i class=\"fas fa-chevron-down\"></i>";
+        check = 0;
+    }
+})
