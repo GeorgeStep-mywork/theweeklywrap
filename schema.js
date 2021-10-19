eac24150264a65ebac78f2fc2bd5f0637e715dbd
GeorgeStep-mywork <71914792+GeorgeStep-mywork@users.noreply.github.com>
2021-10-19 19:25:13 +0530

    Post Preview

--- schema.js
+++ schema.js
@@ -8,7 +8,7 @@
 	"logo": "https://theweeklywrapblog.com/img/My%20Post%20(4).png",
 	"url": "http://theweeklywrapblog.com/",
 	"telephone": "+91 9544967011",
-	"sameAs": ["https://twitter.com/theweeklywrapbl","https://www.linkedin.com/company/theweeklywrapblog/","https://www.instagram.com/theweeklywrapblog/"],
+	"sameAs": ["https://twitter.com/theweeklywrapbl","https://www.linkedin.com/company/theweeklywrapblog/","https://www.instagram.com/improvementbytes/"],
 	"address": {
 		"@type": "PostalAddress",
 		"addressCountry": "Singapore"
